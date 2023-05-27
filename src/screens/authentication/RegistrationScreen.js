a7ae268f1fa8fd3d37190c53352308a719fa6b68
Hoàng Anh <iamnguyenhoanganh@outlook.com>
2023-05-27 10:09:01 +0700

    upload source code

--- src/screens/authentication/RegistrationScreen.js
+++ src/screens/authentication/RegistrationScreen.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import {Text, View} from "react-native";
+import React from "react";
+import { Text, View } from "react-native";
 import Styles from "../../styles/Style";
-import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
-import {TextInputBox} from "../../components/TextInputBox";
-import {ClickableButton} from "../../components/ClickableButton";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { TextInputBox } from "../../components/TextInputBox";
+import { ClickableButton } from "../../components/ClickableButton";
 import Style from "../../styles/Style";
-import { TextInput } from 'react-native-paper'
-
+import { TextInput } from "react-native-paper";
 
 /**
  * Registration Screen
@@ -22,46 +21,67 @@ import { TextInput } from 'react-native-paper'
  * @param onFooterLinkPress footer button listener function
  * @returns {JSX.Element} registration view
  */
-export function RegistrationScreen({setFullName, fullName, setEmail, email, setPassword, password, setConfirmPassword, confirmPassword, onRegisterPress, onFooterLinkPress}){
-    return (
-        <View style={Styles.container}>
-            <KeyboardAwareScrollView
-                style={{ flex: 1, width: '100%' }}
-                keyboardShouldPersistTaps="always">
+export function RegistrationScreen({
+  setFullName,
+  fullName,
+  setEmail,
+  email,
+  setPassword,
+  password,
+  setConfirmPassword,
+  confirmPassword,
+  onRegisterPress,
+  onFooterLinkPress,
+}) {
+  return (
+    <View style={Styles.container}>
+      <KeyboardAwareScrollView
+        style={{ flex: 1, width: "100%" }}
+        keyboardShouldPersistTaps="always"
+      >
+        {/* Text input fields */}
+        <TextInputBox
+          placeholder={"Full Name"}
+          textSetter={setFullName}
+          value={fullName}
+        />
+        <TextInputBox
+          placeholder={"E-mail"}
+          textSetter={setEmail}
+          value={email}
+        />
 
-                {/* Text input fields */}
-                <TextInputBox placeholder={'Full Name'} textSetter={setFullName} value={fullName}/>
-                <TextInputBox placeholder={'E-mail'} textSetter={setEmail} value={email}/>
+        <TextInput
+          label="Password"
+          value={password}
+          mode="outlined"
+          onChangeText={(text) => setPassword(text)}
+          style={Style.input}
+        />
 
+        <TextInput
+          label="Confirm password"
+          value={confirmPassword}
+          mode="outlined"
+          onChangeText={(text) => setConfirmPassword(text)}
+          style={Style.input}
+        />
 
+        {/* Continue Button */}
+        <ClickableButton
+          buttonText={"Continue"}
+          onPressMethod={onRegisterPress}
+        />
 
-                <TextInput
-                    label="Password"
-                    value={password}
-                    mode="outlined"
-                    onChangeText={(text) => setPassword(text)}
-                    style={Style.input}
-                />
-
-                <TextInput
-                    label="Confirm password"
-                    value={confirmPassword}
-                    mode="outlined"
-                    onChangeText={(text) => setConfirmPassword(text)}
-                    style={Style.input}
-                />
-
-
-
-
-                {/* Continue Button */}
-                <ClickableButton buttonText={"Continue"} onPressMethod={onRegisterPress}/>
-
-                {/* Footer Link */}
-                <View style={Styles.footerView}>
-                    <Text style={Styles.footerText}><Text onPress={onFooterLinkPress} style={Styles.footerLink}>Already have an account? Log in</Text></Text>
-                </View>
-            </KeyboardAwareScrollView>
+        {/* Footer Link */}
+        <View style={Styles.footerView}>
+          <Text style={Styles.footerText}>
+            <Text onPress={onFooterLinkPress} style={Styles.footerLink}>
+              Already have an account? Log in
+            </Text>
+          </Text>
         </View>
-    );
+      </KeyboardAwareScrollView>
+    </View>
+  );
 }
